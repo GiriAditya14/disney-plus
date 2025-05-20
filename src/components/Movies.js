66e4950235fb77558a1227b1ec1acd26eb20de86
GiriAditya14 <aditya.2004.giri@gmail.com>
2025-05-20 12:42:37 +0530

    commit

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -25,7 +25,7 @@ function Movies() {
                     
                 }
                 
-                <Wrap>
+                {/* <Wrap>
                     <img alt=' ' src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
                 </Wrap>
                 <Wrap>
@@ -36,7 +36,7 @@ function Movies() {
                 </Wrap>
                 <Wrap>
                     <img alt=' ' src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
-                </Wrap>
+                </Wrap> */}
             </Content>
         </Container>
     )
